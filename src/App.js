56190d860a2aee7d6bb0f3845e23399bd6da66d1
Дмитрий Богатырев <dbogatirev@joytech.store>
2025-04-12 15:32:27 +0700

    add Chat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import { Provider } from './components/ui/provider';
 import {WaitingRoom} from "./components/WaitingRoom";
 import {HubConnectionBuilder} from "@microsoft/signalr";
+import {useState} from "react";
+import {Chat} from "./components/Chat";
 
 export default function App() {
+    const [connection, setConnection] = useState(null);
+    const [chatRoom, setChatRoom] = useState("");
+    const [messages, setMessages] = useState([]);
     const joinChat = async (userName, chatRoom) =>
     {
         const connection = new HubConnectionBuilder()
@@ -15,24 +20,22 @@ export default function App() {
             console.log(message);
         });
 
-
-
         try
         {
             await connection.start();
             await connection.invoke("JoinChat", {userName, chatRoom});
-            console.log(connection);
+
+            setConnection(connection);
         }
         catch (error)
         {
             console.log(error);
         }
-    }
-
+    };
 
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <WaitingRoom joinChat={joinChat}></WaitingRoom>
+            {connection ? <Chat messages={messages} chatRoom={chatRoom} closeChat/> : <WaitingRoom joinChat={joinChat}/>}
         </div>
     );
 }
